2397fc9d8fb9c4c12f6683b983043cbc1f707051
Sacha Schreuer <135665955+sachinovitch@users.noreply.github.com>
2023-08-04 04:07:06 +0200

    cleaning code app

--- app.py
+++ app.py
@@ -17,30 +17,30 @@ decisiontree_loaded = pickle.load(pickle_in)
 def read_root():
     return {
         "message": "Provide the appropriate values of the property",
-        "province_choices": list(ProvinceEnum),'Antwerp'|'Brussels'|'East_Flanders'|'Flemish_Brabant'|'Walloon_Brabant'|'Hainaut'|'Limburg'|'Liège'|'Luxembourg'|'Namur'|'West_Flanders'
-        "subtype_choices": list(SubtypeEnum),
-    }
-# "province_choices": "Antwerp | Brussels | East_Flanders | Flemish_Brabant | Walloon_Brabant | Hainaut | Limburg |Liège | Luxembourg | Namur | West_Flanders"
-# def read_root():
-#     return {"message": "provide the appropriate values of the property",
-#             "'total_property_area', 'number_of_facades' and 'number_of_bedrooms' require an integer as value","'state_of_the building', 'furnished', 'open_fire', 'terrace' and 'garden' require a boolean value 'true' or 'false'",
-#             return {
-#         "message": "Provide the appropriate values of the property",
-#         "province_choices": list(ProvinceEnum),
-#         "subtype_choices": list(SubtypeEnum),}
-#     }
-    #"message": "'province' takes one of these strings: 'Antwerp', 'Brussels', 'East_Flanders', 'Flemish_Brabant', 'Walloon_Brabant', 'Hainaut', 'Limburg', 'Liège', 'Luxembourg', 'Namur', 'West_Flanders'",
-            #"message": "'subtype' takes one of these strings: 'APARTMENT','APARTMENT_BLOCK','BUNGALOW','CASTLE','CHALET','COUNTRY_COTTAGE','DUPLEX','EXCEPTIONAL_PROPERTY','FARMHOUSE','FLAT_STUDIO','GROUND_FLOOR','HOUSE','KOT','LOFT','MANOR_HOUSE','MANSION','MIXED_USE_BUILDING','OTHER_PROPERTY','PENTHOUSE','SERVICE_FLAT','TOWN_HOUSE','TRIPLEX','VILLA'"
-            
+        "total_property_area": "integer",
+        "number_of_facades": "integer",
+        "number_of_bedrooms": "integer",
+        "state_of_the_building": "true | false",
+        "furnished": "true | false",
+        "open_fire": "true | false",
+        "terrace": "true | false",
+        "garden": "true | false",
+        "swimming_pool": "true | false",
+        "province_choices": "Antwerp | Brussels | East_Flanders | Flemish_Brabant | Walloon_Brabant | Hainaut | Limburg |Liège | Luxembourg | Namur | West_Flanders",
+        "subtype_choices": "APARTMENT | APARTMENT_BLOCK | BUNGALOW | CASTLE | CHALET | COUNTRY_COTTAGE | DUPLEX | EXCEPTIONAL_PROPERTY | FARMHOUSE | FLAT_STUDIO | GROUND_FLOOR | HOUSE | KOT | LOFT | MANOR_HOUSE | MANSION | MIXED_USE_BUILDING | OTHER_PROPERTY | PENTHOUSE | SERVICE_FLAT | TOWN_HOUSE | TRIPLEX | VILLA"}
+
 @app.post("/predict/")
 def predict_price(data: InputData):
     try:
         # create feature vector
-        X = preprocess_new_data(data)
-        prediction = decisiontree_loaded.predict(X)
+        Feature_Vector = preprocess_new_data(data)
+        prediction = decisiontree_loaded.predict(Feature_Vector)
         return {"prediction": "€"+str(prediction[0]),
                     "status_code": "200"} # return predicted price in euro
+    
     except TypeError:
-          return {"error": "TypeError"}
+          return {"error": "TypeError",
+                  "status_code": "400"}
     except KeyError as e:
-          return {"error": f"11 fields expected. You forgot: {e}."}
+          return {"error": f"11 fields expected. You forgot: {e}.",
+                  "status_code": "400"}
